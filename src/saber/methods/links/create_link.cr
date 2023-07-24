dd577587b559ce02d6dfbf05ad500ad98fcd4bc0
jcads <rafaelcadorna@pm.me>
2023-07-24 23:06:57 +0800

    Build json body

--- src/saber/methods/links/create_link.cr
+++ src/saber/methods/links/create_link.cr
@@ -9,15 +9,29 @@ class Saber::Link
     destination : String? = nil
   ) : Link
     io = IO::Memory.new
-    builder = ParamsBuilder.new(io)
     assets = assets.map(&.to_n_tuple)
 
-    {% for x in %w(creator assets title expires_at article_url organization destination) %}
-      # TODO: convert key to camelcase
-      builder.add({{x}}, {{x.id}}) unless {{x.id}}.nil?
-    {% end %}
+    JSON.build(io) do |json|
+      json.object do
+        {% for x in %w(creator assets title expires_at article_url organization destination) %}
+          if {{x.id}}.nil?
+            # do nothing
+          elsif {{x.id}}.is_a?(Array)
+            json.field {{x}}.camelcase(lower: true) do
+              json.array do
+                {{x.id}}.each do |el|
+                  json.raw(el.to_json)
+                end
+              end
+            end
+          else
+            json.field {{x}}.camelcase(lower: true), {{x.id}}
+          end
+        {% end %}
+      end
+    end
 
-    response = Saber.client.post("/api/links/link/", form: io.to_s)
+    response = Saber.client.post("/api/links/link/", body: io.to_s)
 
     if response.status_code == 200
       Link.from_json(response.body)
