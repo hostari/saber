dd3d9811f46425276a8e160234401fc18937879c
jcads <rafaelcadorna@pm.me>
2023-07-25 09:17:37 +0800

    Add trailing slash to url path

--- src/saber/methods/links/retrieve_link.cr
+++ src/saber/methods/links/retrieve_link.cr
@@ -1,6 +1,6 @@
 class Saber::Link
   def self.retrieve(short_url : String) : Link
-    response = Saber.client.get("/api/links/link/#{short_url}")
+    response = Saber.client.get("/api/links/link/#{short_url}/")
 
     if response.status_code == 200
       Link.from_json(response.body)
